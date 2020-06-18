31c185d6cac07ab9fdcb22aaa6fc93790873de85
MaoMao-arch <17377202553@163.com>
2020-06-18 16:11:14 +0800

    搜索框优化

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,8 +4,11 @@ const app = getApp()
 
 Page({
   data: {
-    searchHidden: false,
-    searchPageShow: false,
+    searchHidden: false, //顶部搜索框是否隐藏
+    searchPageShow: false, //遮罩层是否显示
+    searchValue: '', //搜索框的值
+    showAction: true, //是否在搜索框右侧显示取消按钮
+    useActionSlot: false, //是否显示搜索框中的搜索按钮
   },
 
   //下方菜单显示
@@ -41,5 +44,25 @@ Page({
     this.setData({
       searchPageShow: false
     })
+  },
+
+  //点击搜索 
+  onSearch() {
+    console.log(this.data.searchValue)
+  },
+
+  //搜索框变化
+  onSearchChange(e) {
+    if (e.detail !== "") {
+      this.setData({
+        showAction: false, //是否在搜索框右侧显示取消按钮
+        useActionSlot: true, //是否显示搜索框中的搜索按钮
+      })
+    } else {
+      this.setData({
+        showAction: true, //是否在搜索框右侧显示取消按钮
+        useActionSlot: false, //是否显示搜索框中的搜索按钮
+      })
+    }
   }
 })
